02d4fb4fd0575cc928bd21b0b6f28e7e4a94430d
Robert <robertpawelh@gmail.com>
2020-03-31 14:38:42 +0200

    Little bugfixes

--- server.py
+++ server.py
@@ -8,65 +8,76 @@ cards_filename = "data/cards.json"
 terminals_filename = "data/terminals.json"
 registrations_filename = "data/registrations.json"
 
+
 def read_data(path):
-    with open (path) as f:
+    with open(path) as f:
         data = json.load(f)
     logger.log(str(path) + " loaded")
     return data
 
+
 def write_data(path, data):
-   with open (path, 'w') as f:
+    with open(path, 'w') as f:
         json.dump(data, f, indent=2)
+
 
 workers = read_data(workers_filename)
 cards = read_data(cards_filename)
 terminals = read_data(terminals_filename)
 registrations = read_data(registrations_filename)
 
-def add_terminal(name, terminal_id):
-    if terminal_id not in terminals:
-        terminals[terminal_id] = {'name': name}
-        write_data(terminals_filename, terminals)
+
+def add_name_to_id(path, dictionary, name, id):
+    if id not in dictionary:
+        dictionary[id] = {'name': name}
+        write_data(path, dictionary)
         logger.log(f"{name} added")
     else:
-        logger.log("Terminal already exists")
+        logger.log(f"{id} already exists")
 
-def remove_terminal(terminal_id):
-    terminals.remove(terminal_id)
-    write_data(terminals_filename, terminals)
-    logger.log(f"Terminal with {terminal_id} removed")
 
-def add_card(name, card_id):
-    cards[card_id] = {'name': name}
-    write_data(cards_filename, cards)
-    logger.log(f"{name} added")
+def remove_data(path, dictionary, id):
+    if id in dictionary:
+        del dictionary[id]
+        write_data(path, dictionary)
+        logger.log(f"{id} removed")
+    else:
+        logger.log(f"Incorrect id")
 
-def remove_card(card_id):
-    cards.remove(card_id)
-    write_data(cards_filename, cards)
-    logger.log(f"Card with {card_id} removed")
 
 def add_worker(name):
     id = uuid.uuid1().int
-    worker = {'name': name}
-    workers[id] = worker
-    write_data(workers_filename, workers)
-    logger.log(f"{name} added")
+    add_name_to_id(workers_filename, workers, name, id)
+
 
 def remove_worker(worker_id):
-    if worker_id in workers:
-        del workers[worker_id]
-        write_data(workers_filename, workers)
-        logger.log(f"Worker with {worker_id} removed")
-    else:
-        logger.log(f"Incorrect id")
-def assing_card_id(worker_id, card_id):
+    remove_data(workers_filename, workers, worker_id)
+
+
+def add_card(name, card_id):
+    add_name_to_id(cards_filename, cards, name, card_id)
+
+
+def remove_card(card_id):
+    remove_data(cards_filename, cards, card_id)
+
+
+def add_terminal(name, terminal_id):
+    add_name_to_id(terminals_filename, terminals, name, terminal_id)
+
+
+def remove_terminal(terminal_id):
+    remove_data(terminals_filename, terminals, terminal_id)
+
+
+def assign_card_id(worker_id, card_id):
     if worker_id in workers and card_id in cards:
         workers[worker_id]['card_id'] = card_id
         write_data(workers_filename, workers)
         logger.log(f"{card_id} assigned to {worker_id}")
     else:
         logger.log("Operation didn't succeed")
+
 
 def disassign_card_id(worker_id):
     if worker_id in workers:
@@ -77,25 +88,28 @@ def disassign_card_id(worker_id):
     else:
         logger.log("Worker doesn't exist")
 
+
 def registration(card_id, terminal_id):
-    flag = False
     worker = None
     for worker_id, worker_data in workers.items():
-        if 'cardId' in worker_data:
-            if card_id==worker_data['card_id']:
+        if 'card_id' in worker_data:
+            if card_id == worker_data['card_id']:
                 worker = worker_id
     date = str(datetime.datetime.now())
     if worker:
-        registrations.append({'worker_id': worker_id, 'date': date, 'terminal_id': terminal_id})
+        registrations.append({'worker_id': worker, 'card_id': card_id, 'date': date, 'terminal_id': terminal_id})
+        logger.log(f"New registration at {date}")
     else:
         registrations.append({'card_id': card_id, 'date': date, 'terminal_id': terminal_id})
+        logger.log(f"New unidentified registration at {date}")
     write_data(registrations_filename, registrations)
-    logger.log(f"New unidentified registration at {date}")
 
 def generate_raport(client):
     pass
 
+
 def hard_reset():
+    global terminals, cards, workers, registrations
     terminals = {}
     cards = {}
     workers = {}
@@ -105,6 +119,7 @@ def hard_reset():
     write_data(workers_filename, workers)
     write_data(registrations_filename, registrations)
     logger.log("Deleted all data")
+
 
 """ RASPBERRY PI METHOD TO SERVICE RFID
 import time
