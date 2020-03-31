7f0b5e311b8556d1dac006557d9f4a7d2927c704
Robert <robertpawelh@gmail.com>
2020-03-31 13:21:19 +0200

    Change data structures and add logs

--- emulator.py
+++ emulator.py
@@ -3,12 +3,14 @@ import server
 
 flag = True
 keyboard.add_hotkey('1', lambda: server.add_worker("Seba"))
-keyboard.add_hotkey('2', lambda: server.add_terminal(1234))
-keyboard.add_hotkey('tab', lambda: server.add_card(643))
-keyboard.add_hotkey('3', lambda: server.assing_card_id("328377771909487252715599941261395726277", 12345))
-keyboard.add_hotkey('4', lambda: server.remove_card_id("328377771909487252715599941261395726277"))
-keyboard.add_hotkey('5', lambda: server.registration(12345, 123432))
-keyboard.add_hotkey('6', lambda: server.registration(123456, 123432))
+keyboard.add_hotkey('q', lambda: server.remove_worker("224235145077410045823478833888085905349"))
+keyboard.add_hotkey('2', lambda: server.add_terminal("Terminal 1", "1234"))
+keyboard.add_hotkey('tab', lambda: server.add_card("Card 1", "643"))
+keyboard.add_hotkey('3', lambda: server.assing_card_id("224235145077410045823478833888085905349", "643"))
+keyboard.add_hotkey('4', lambda: server.disassign_card_id("224235145077410045823478833888085905349"))
+keyboard.add_hotkey('5', lambda: server.registration("643", "123432"))
+keyboard.add_hotkey('6', lambda: server.registration("123456", "123432"))
+keyboard.add_hotkey('r', lambda: server.hard_reset())
 
 while(flag):
     if (keyboard.is_pressed('Esc')):
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 import datetime
+import logger
 
 workers_filename = "data/workers.json"
 cards_filename = "data/cards.json"
@@ -10,6 +11,7 @@ registrations_filename = "data/registrations.json"
 def read_data(path):
     with open (path) as f:
         data = json.load(f)
+    logger.log(str(path) + " loaded")
     return data
 
 def write_data(path, data):
@@ -21,59 +23,88 @@ cards = read_data(cards_filename)
 terminals = read_data(terminals_filename)
 registrations = read_data(registrations_filename)
 
-def add_terminal(terminal_id):
-    terminal = {'terminalId': terminal_id}
-    terminals.append(terminal)
-    write_data(terminals_filename, terminals)
+def add_terminal(name, terminal_id):
+    if terminal_id not in terminals:
+        terminals[terminal_id] = {'name': name}
+        write_data(terminals_filename, terminals)
+        logger.log(f"{name} added")
+    else:
+        logger.log("Terminal already exists")
 
 def remove_terminal(terminal_id):
     terminals.remove(terminal_id)
     write_data(terminals_filename, terminals)
+    logger.log(f"Terminal with {terminal_id} removed")
 
-def add_card(card_id):
-    card = {'cardId': card_id}
-    cards.append(card)
+def add_card(name, card_id):
+    cards[card_id] = {'name': name}
     write_data(cards_filename, cards)
+    logger.log(f"{name} added")
 
 def remove_card(card_id):
     cards.remove(card_id)
     write_data(cards_filename, cards)
+    logger.log(f"Card with {card_id} removed")
 
 def add_worker(name):
     id = uuid.uuid1().int
     worker = {'name': name}
     workers[id] = worker
     write_data(workers_filename, workers)
+    logger.log(f"{name} added")
 
-def remove_worker(name):
-    workers.remove(id)
-    write_data(workers_filename, workers)
+def remove_worker(worker_id):
+    if worker_id in workers:
+        del workers[worker_id]
+        write_data(workers_filename, workers)
+        logger.log(f"Worker with {worker_id} removed")
+    else:
+        logger.log(f"Incorrect id")
+def assing_card_id(worker_id, card_id):
+    if worker_id in workers and card_id in cards:
+        workers[worker_id]['card_id'] = card_id
+        write_data(workers_filename, workers)
+        logger.log(f"{card_id} assigned to {worker_id}")
+    else:
+        logger.log("Operation didn't succeed")
 
-def assing_card_id(workerId, card_id):
-    workers[workerId]['cardId'] = card_id
-    write_data(workers_filename, workers)
-
-def remove_card_id(worker_id):
-    worker = {'name': workers[worker_id]['name']}
-    workers[worker_id] = worker
-    write_data(workers_filename, workers)
+def disassign_card_id(worker_id):
+    if worker_id in workers:
+        worker = {'name': workers[worker_id]['name']}
+        workers[worker_id] = worker
+        write_data(workers_filename, workers)
+        logger.log(f"{worker_id} now doesn't have card_id")
+    else:
+        logger.log("Worker doesn't exist")
 
 def registration(card_id, terminal_id):
     flag = False
     worker = None
     for worker_id, worker_data in workers.items():
         if 'cardId' in worker_data:
-            if card_id==worker_data['cardId']:
+            if card_id==worker_data['card_id']:
                 worker = worker_id
+    date = str(datetime.datetime.now())
     if worker:
-        registrations.append({'worker_id': worker_id, 'date': str(datetime.datetime.now()), 'terminalId': terminal_id})
+        registrations.append({'worker_id': worker_id, 'date': date, 'terminal_id': terminal_id})
     else:
-        registrations.append({'card_id': card_id, 'date': str(datetime.datetime.now()), 'terminalId': terminal_id})
+        registrations.append({'card_id': card_id, 'date': date, 'terminal_id': terminal_id})
     write_data(registrations_filename, registrations)
+    logger.log(f"New unidentified registration at {date}")
 
 def generate_raport(client):
     pass
 
+def hard_reset():
+    terminals = {}
+    cards = {}
+    workers = {}
+    registrations = []
+    write_data(terminals_filename, terminals)
+    write_data(cards_filename, cards)
+    write_data(workers_filename, workers)
+    write_data(registrations_filename, registrations)
+    logger.log("Deleted all data")
 
 """ RASPBERRY PI METHOD TO SERVICE RFID
 import time
